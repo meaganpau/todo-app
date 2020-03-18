aa9d9a9ab3f0433245b285f8a65512a5369eb437
Meagan Pau <meagan.pau@crowdlinker.com>
2020-03-18 17:08:08 -0400

    Updates

---
+++ precache-manifest.63670106955db991a903e2010ebdd4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e642030410418bc461ab7c3ec202555",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "456ce940ff40666d8831",
+    "url": "/todo-app/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "456ce940ff40666d8831",
+    "url": "/todo-app/static/js/2.b63083e3.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "/todo-app/static/js/2.b63083e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0238961561e3e2d5d6ef",
+    "url": "/todo-app/static/js/main.00aad46a.chunk.js"
+  },
+  {
+    "revision": "48e7249bb752287fbef5",
+    "url": "/todo-app/static/js/runtime-main.21a27cdb.js"
+  }
+]);
